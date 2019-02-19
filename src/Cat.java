8cb62f795c9bd7c2e6ca41e25c7cf35ff57076d6
koldointxaustiusername <ik013043z1@H013043.NET>
2019-02-19 13:41:48 +0100

    v1

---
+++ src/Cat.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+public class Cat {
+	public static void cat(File file) {
+	    RandomAccessFile input = null;
+	    String line = null;
+
+	    try {
+	        input = new RandomAccessFile(file, "r");
+	        while ((line = input.readLine()) != null) {
+	            System.out.println(line);
+	        }
+	        return;
+	    } catch (IOException e){
+	    	e.printStackTrace();
+	    }finally {
+	        if (input != null) {
+	            try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	        }
+	    }
+	}
+	
+}
